0f2c641ffc4c58a33a89b8b7e04709ec4d3b1321
Nils Achterholt <nils.achterholt@profil.com>
2026-01-20 13:44:01 +0100

    add more aliases

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -3,3 +3,12 @@ alias ll="eza -lh --icons"
 alias la="eza -lha --icons"
 
 alias cat="bat"
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+
+alias ..="cd .."
+
+alias grep="rg"
